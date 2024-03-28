b527a35bdb43746d10398325015c43e57653c4d6
0xdk <dineshrk394@gmail.com>
2024-03-28 07:12:55 +0530

    refactor

--- utils/priceDIfference.js
+++ utils/priceDIfference.js
@@ -1,51 +1,40 @@
 // Calculates price changes based on yesterday and today's prices for 24K and 22K gold
-function getPriceChangeInfo(data) {
+function getPriceDifferenceInfo(yesterdayDoc, todayDoc) {
   // for 24k
-  let todayPrice24k = parseInt(
-    data[data.length - 1].priceArray[1].replace(/,/g, ''),
-    10
-  );
+  let todayPrice24k = parseInt(todayDoc[1].replace(/,/g, ''), 10);
 
   let yesterdayPrice24k = parseInt(
-    data[data.length - 2].priceArray[1].replace(/,/g, ''),
+    yesterdayDoc.priceArray[1].replace(/,/g, ''),
     10
   );
   let difference24k = todayPrice24k - yesterdayPrice24k;
 
   // for 22k
-  let todayPrice22k = parseInt(
-    data[data.length - 1].priceArray[3].replace(/,/g, ''),
+  let todayPrice22k = parseInt(todayDoc[3].replace(/,/g, ''), 10);
+
+  let yesterdayPrice22k = parseInt(
+    yesterdayDoc.priceArray[3].replace(/,/g, ''),
     10
   );
 
-  let yesterdayPrice22k = parseInt(
-    data[data.length - 2].priceArray[3].replace(/,/g, ''),
-    10
-  );
   let difference22k = todayPrice22k - yesterdayPrice22k;
 
   if (difference24k > 0 || difference22k > 0) {
     return {
-      status: 'up',
       change: [difference24k, difference22k],
       symbol: '+',
-      message: `The price went up.`,
     };
   } else if (difference24k < 0 || difference22k < 0) {
     return {
-      status: 'down',
       change: [Math.abs(difference24k), Math.abs(difference22k)],
       symbol: '-',
-      message: `The price went down.`,
     };
   } else {
     return {
-      status: 'unchanged',
       change: [0, 0],
       symbol: '',
-      message: 'The price remained unchanged.',
     };
   }
 }
 
-module.exports = { getPriceChangeInfo };
+module.exports = { getPriceDifferenceInfo };
